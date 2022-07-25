bce3feebf4c2a7059e04ecd8a9b91eb93088a084
Tikoz <106456046+Tikozaal@users.noreply.github.com>
2022-07-25 13:00:45 +0200

    Add files via upload

---
+++ Tikoz_Mecano/server.lua
@@ -0,0 +1,234 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+TriggerEvent('esx_society:registerSociety', 'mechanic', 'mechanic', 'society_mechanic', 'society_mechanic', 'society_mechanic', {type = 'public'})
+
+
+TriggerEvent('esx_addonaccount:getSharedAccount', 'society_mechanic', function(account)
+	societyAccount = account
+end)
+
+RegisterServerEvent('Tikoz:GarageOuvert')
+AddEventHandler('Tikoz:GarageOuvert', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local xPlayers	= ESX.GetPlayers()
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], "Benny's", '~b~Annonce', 'Nous sommes ~g~ouvert~s~ !', 'CHAR_CARSITE3', 8)
+	end
+end)
+
+RegisterServerEvent('Tikoz:GarageFermer')
+AddEventHandler('Tikoz:GarageFermer', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local xPlayers	= ESX.GetPlayers()
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], "Benny's", '~b~Annonce', 'Nous sommes ~r~fermé~s~ !', 'CHAR_CARSITE3', 8)
+	end
+end)
+
+RegisterServerEvent('Tikoz:MecanoMsgPerso')
+AddEventHandler('Tikoz:MecanoMsgPerso', function(msgpersomecano)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local xPlayers	= ESX.GetPlayers()
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], "Benny's", '~b~Annonce', msgpersomecano, 'CHAR_CARSITE3', 8)
+	end
+end)
+
+RegisterServerEvent('Tikoz:Recruter')
+AddEventHandler('Tikoz:Recruter', function(target, job, grade)
+	local _source = source
+	local sourceXPlayer = ESX.GetPlayerFromId(_source)
+	local targetXPlayer = ESX.GetPlayerFromId(target)
+	targetXPlayer.setJob(job, grade)
+	TriggerClientEvent('esx:showNotification', _source, "Vous avez ~g~recruté " .. targetXPlayer.name .. "~w~.")
+	TriggerClientEvent('esx:showNotification', target, "Vous avez été ~g~embauché par " .. sourceXPlayer.name .. "~w~.")
+end)
+
+RegisterServerEvent('Tikoz:PromotionMecano')
+AddEventHandler('Tikoz:PromotionMecano', function(target)
+	local _source = source
+
+	local sourceXPlayer = ESX.GetPlayerFromId(_source)
+	local targetXPlayer = ESX.GetPlayerFromId(target)
+
+	if (targetXPlayer.job.grade == 3) then
+		TriggerClientEvent('esx:showNotification', _source, "Vous ne pouvez pas plus ~b~promouvoir~w~ d'avantage.")
+	else
+		if (sourceXPlayer.job.name == targetXPlayer.job.name) then
+			local grade = tonumber(targetXPlayer.job.grade) + 1
+			local job = targetXPlayer.job.name
+
+			targetXPlayer.setJob(job, grade)
+
+			TriggerClientEvent('esx:showNotification', _source, "Vous avez ~b~promu " .. targetXPlayer.name .. "~w~.")
+			TriggerClientEvent('esx:showNotification', target, "Vous avez été ~b~promu~s~ par " .. sourceXPlayer.name .. "~w~.")
+		end
+	end
+end)
+
+
+RegisterServerEvent('Tikoz:Retrograder')
+AddEventHandler('Tikoz:Retrograder', function(target)
+	local _source = source
+
+	local sourceXPlayer = ESX.GetPlayerFromId(_source)
+	local targetXPlayer = ESX.GetPlayerFromId(target)
+
+	if (targetXPlayer.job.grade == 0) then
+		TriggerClientEvent('esx:showNotification', _source, "Vous ne pouvez pas plus ~r~rétrograder~w~ d'avantage.")
+	else
+		if (sourceXPlayer.job.name == targetXPlayer.job.name) then
+			local grade = tonumber(targetXPlayer.job.grade) - 1
+			local job = targetXPlayer.job.name
+
+			targetXPlayer.setJob(job, grade)
+
+			TriggerClientEvent('esx:showNotification', _source, "Vous avez ~r~rétrogradé " .. targetXPlayer.name .. "~w~.")
+			TriggerClientEvent('esx:showNotification', target, "Vous avez été ~r~rétrogradé par " .. sourceXPlayer.name .. "~w~.")
+		else
+			TriggerClientEvent('esx:showNotification', _source, "Vous n'avez pas ~r~l'autorisation~w~.")
+		end
+	end
+end)
+
+RegisterServerEvent('Tikoz:Virer')
+AddEventHandler('Tikoz:Virer', function(target)
+	local _source = source
+	local sourceXPlayer = ESX.GetPlayerFromId(_source)
+	local targetXPlayer = ESX.GetPlayerFromId(target)
+	local job = "unemployed"
+	local grade = "0"
+	if (sourceXPlayer.job.name == targetXPlayer.job.name) then
+		targetXPlayer.setJob(job, grade)
+		TriggerClientEvent('esx:showNotification', _source, "Vous avez ~r~viré " .. targetXPlayer.name .. "~w~.")
+		TriggerClientEvent('esx:showNotification', target, "Vous avez été ~g~viré par " .. sourceXPlayer.name .. "~w~.")
+	else
+		TriggerClientEvent('esx:showNotification', _source, "Vous n'avez pas ~r~l'autorisation~w~.")
+	end
+end)
+
+
+
+RegisterServerEvent('Tikoz:Bennysretirecoffre')
+AddEventHandler('Tikoz:Bennysretirecoffre', function(itemName, count)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local sourceItem = xPlayer.getInventoryItem(itemName)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_mechanic', function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+
+		if count > 0 and inventoryItem.count >= count then
+
+			if sourceItem.limit ~= -1 and (sourceItem.count + count) > sourceItem.limit then
+				TriggerClientEvent('esx:showNotification', _source, "quantité invalide")
+			else
+				inventory.removeItem(itemName, count)
+				xPlayer.addInventoryItem(itemName, count)
+				TriggerClientEvent('esx:showNotification', _source, "Vous avez retiré ~y~x"..count.."~b~ "..itemName)
+			end
+		else
+			TriggerClientEvent('esx:showNotification', _source, "quantité invalide")
+		end
+	end)
+end)
+
+
+RegisterNetEvent('Tikoz:BennysDeposeItem')
+AddEventHandler('Tikoz:BennysDeposeItem', function(itemName, count)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local sourceItem = xPlayer.getInventoryItem(itemName)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_mechanic', function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+
+		if sourceItem.count >= count and count > 0 then
+			xPlayer.removeInventoryItem(itemName, count)
+			inventory.addItem(itemName, count)
+			TriggerClientEvent('esx:showNotification', _source, "Objet déposé ~y~x"..count.."~b~ "..itemName.."")
+		else
+			TriggerClientEvent('esx:showNotification', _source, "quantité invalide")
+		end
+	end)
+end)
+
+
+ESX.RegisterServerCallback('Tikoz:inventairejoueur', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local items   = xPlayer.inventory
+
+	cb({items = items})
+end)
+
+ESX.RegisterServerCallback('Tikoz:Bennysprendreitem', function(source, cb)
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_mechanic', function(inventory)
+		cb(inventory.items)
+	end)
+end)
+
+RegisterServerEvent('Tikoz:AlertMecano')
+AddEventHandler('Tikoz:AlertMecano', function() 
+	local xPlayers = ESX.GetPlayers()
+    for i = 1, #xPlayers do
+        local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+        if xPlayer.job.name == 'mechanic' then
+			TriggerClientEvent('Tikoz:alertmeca', xPlayer.source)
+		end
+	end
+end)
+
+
+ESX.RegisterServerCallback('Tikoz:getSocietyMoney', function(source, cb, societyName)
+	if societyName ~= nil then
+	  local society = "society_mechanic"
+	  TriggerEvent('esx_addonaccount:getSharedAccount', society, function(account)
+		cb(account.money)
+	  end)
+	else
+	  cb(0)
+	end
+end)
+
+
+RegisterServerEvent("Tikoz:Bennysdepotentreprise")
+AddEventHandler("Tikoz:Bennysdepotentreprise", function(money)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    local total = money
+    local xMoney = xPlayer.getMoney()
+    
+    TriggerEvent('esx_addonaccount:getSharedAccount', "society_mechanic", function (account)
+        if xMoney >= total then
+            account.addMoney(total)
+            xPlayer.removeAccountMoney('bank', total)
+            TriggerClientEvent('esx:showAdvancedNotification', source, 'Banque Société', "~b~Benny's", "Vous avez déposé ~g~"..total.." $~s~ dans votre ~b~entreprise", 'CHAR_BANK_FLEECA', 9)
+        else
+            TriggerClientEvent('esx:showNotification', source, "<C>~r~Vous n'avez pas assez d\'argent !")
+        end
+    end)   
+end)
+
+RegisterServerEvent("Tikoz:BennysRetraitEntreprise")
+AddEventHandler("Tikoz:BennysRetraitEntreprise", function(money)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	local total = money
+	local xMoney = xPlayer.getAccount("bank").money
+	
+	TriggerEvent('esx_addonaccount:getSharedAccount', "society_mechanic", function (account)
+		if account.money >= total then
+			account.removeMoney(total)
+			xPlayer.addAccountMoney('bank', total)
+			TriggerClientEvent('esx:showAdvancedNotification', source, 'Banque Société', "~b~Benny's", "Vous avez retiré ~g~"..total.." $~s~ de votre ~b~entreprise", 'CHAR_BANK_FLEECA', 9)
+		else
+			TriggerClientEvent('esx:showNotification', source, "~r~Vous n'avez pas assez d\'argent dans votre entreprise!")
+		end
+	end)
+end) 
