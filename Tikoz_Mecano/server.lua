59413af931d0791715bc02686975c225999728a5
Tikoz <106456046+Tikozaal@users.noreply.github.com>
2022-09-19 17:15:57 +0200

    Update server.lua

--- Tikoz_Mecano/server.lua
+++ Tikoz_Mecano/server.lua
@@ -248,7 +248,7 @@ AddEventHandler("Tikoz:mecanodepotentreprise", function(money)
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
     local total = money
-    local xMoney = xPlayer.getMoney()
+    local xMoney = xPlayer.getAccount("bank").money
     
     TriggerEvent('esx_addonaccount:getSharedAccount', "society_mechanic", function (account)
         if xMoney >= total then
