611b9e05849dc5a643e8f6049b86cc940bac9b0f
Tikoz <106456046+Tikozaal@users.noreply.github.com>
2023-01-24 23:09:23 +0100

    Add files via upload

---
+++ Tikoz_Mecano/config.lua
@@ -0,0 +1,50 @@
+Config = {}
+
+Config.Pos = {
+
+	Fourriere = vector3(409.57, -1622.99, 28.29),
+
+	SpawnFourriere = vector4(417.05, -1627.54, 28.29, 143.9),
+
+    Garage = vector3(-186.94, -1310.13, 30.3),
+
+	SpawnVehicule = vector4(-183.74, -1317.55, 31.3, 1.011),
+
+	DeleteVehicle = vector3(-183.74, -1317.55, 30.3),
+
+    Acceuil = vector3(-210.95, -1309.63, 30.29),
+
+    Vestiaire = vector3(-224.37, -1320.52, 29.89),
+
+    Coffre = vector3(-216.26, -1319.02, 29.89),
+
+    Boss = vector3(-213.4, -1334.77, 33.89),
+
+	BossPolice = vector3(448.28, -973.18, 29.69),
+
+    Blip = vector3(-216.26, -1319.02, 29.89)
+
+}
+
+GarageList = {
+	{name = "Dépanneuse", label = "sultan"},
+	{name = "Plateau", label = "towtruck"}
+}
+
+Config.Tenue = {
+	Homme = {
+	['tshirt_1'] = 15, ['tshirt_2'] = 0,
+	['torso_1'] = 65, ['torso_2'] = 0,
+	['arms'] = 0,
+	['pants_1'] = 38, ['pants_2'] = 0,
+	['shoes_1'] = 25, ['shoes_2'] = 0,
+	},
+
+	Femme = {
+	['tshirt_1'] = 38,['tshirt_2'] = 0,
+	['torso_1'] = 57, ['torso_2'] = 0,
+	['arms'] = 21, ['arms_2'] = 0,
+	['pants_1'] = 11, ['pants_2'] = 1,
+	['shoes_1'] = 49, ['shoes_2'] = 0,
+	}
+}
